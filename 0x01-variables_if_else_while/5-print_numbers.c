5c335eca4b9c873fb9c90a43c7358a14e26c2cf9
AquillaAf <aquillaafintinni@gmail.com>
2023-08-12 00:25:26 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * a]main - Entry point
+ * Description: 'print base 10 numbers'
+ * Return: always 0
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		printf("%d", k);
+	}
+	printf("\n");
+	return (0);
+}
