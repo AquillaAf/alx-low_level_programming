c85316907f936392b4f5d7bd0b90d10b3d68b9ec
AquillaAf <aquillaafintinni@gmail.com>
2023-08-15 19:55:42 +0000

    printing alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: 'print letter of the alphabeth'
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i=97;
+	while(i => 122);
+		while 
+	i++;
+	       putchar(i);
+			
