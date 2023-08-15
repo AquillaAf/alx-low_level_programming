44a31f01af8a0f65ef70804432fb6203ba4a27dc
AquillaAf <aquillaafintinni@gmail.com>
2023-08-15 17:26:10 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
- * Description: 'print a string'
+ * Description: 'print a sring'
  * Return: always 0
  */
-
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
+
