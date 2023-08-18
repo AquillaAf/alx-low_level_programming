e4888614ef0909f05c389cb3899c0efb0d0783e7
AquillaAf <aquillaafintinni@gmail.com>
2023-08-18 02:55:46 +0000

    multiple numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - function to multiply an array
+ *
+ */
+void more_numbers(void)
+{
+	int k;
+
+	for (k = 1; k <= 10; k++)
+	{
+		int i = 0;
+		while (i <= 14)
+		i++;
+		_putchar (i);
+	}
+	_putchar ('\n');
+}
