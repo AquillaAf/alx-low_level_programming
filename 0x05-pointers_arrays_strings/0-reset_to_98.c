d72c7806a4fa1a52d04a6744c6b99c3e87228ed5
AquillaAf <aquillaafintinni@gmail.com>
2023-08-22 00:21:44 +0000

    swaping int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that points a parameter and change its value
+ * @n: Argument that serves as pointer address
+ * return: Always 0
+ *
+ */
+void reset_to_98(int *n)
+{
+
+*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - function that sawp the value of two integers
+ * @a: value of first integer
+ * @b: value of second integer
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
