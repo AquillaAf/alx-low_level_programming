0e9840f337b760f4f66bae9c65cf9254b06ff182
AquillaAf <aquillaafintinni@gmail.com>
2023-08-22 02:18:45 +0000

    int swaping

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,19 @@
+int _strlen(char *s);
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
