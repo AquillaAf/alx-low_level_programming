69642f4d5119644251fe0532b8cfeb88f187432b
AquillaAf <aquillaafintinni@gmail.com>
2023-08-31 22:39:21 +0000

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,7 +14,7 @@ int is_prime_number(int n)
 
 /**
  * cp - check all numbers < n if tghey divide it
- * @n: int 
+ * @n: int
  * @tool: int
  *
  * Return: int
@@ -26,8 +26,10 @@ int cp(int n, int tool)
 		return (1);
 	}
 	else if (n % tool == 0 || n <= 1)
+	{
 		return (0);
-	else 
+	}
+	else
 	{
 		return (cp(n, tool + 1));
 	}
