c79c825ece79b6b1b8f19383cee6b9fbbc82ee09
AquillaAf <aquillaafintinni@gmail.com>
2023-08-31 13:46:31 +0000

    recursive string printing

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 void _puts_recursion(char *s)
 {
 
-	if (s[0] != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[0]);
-		_puts_recursion(&s[1]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	if (s[1] == '\0')
+	else
 	{
 		_putchar ('\n');
 	}
