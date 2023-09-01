8b96af6d20ee8f27e6bbb7fac4f01842aab44a9d
AquillaAf <aquillaafintinni@gmail.com>
2023-09-01 23:21:06 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function prints its name
+ * @argc: length of command arguments
+ * @argv: its string length
+ *
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	else
+	{
+	}
+return (0);
+}
