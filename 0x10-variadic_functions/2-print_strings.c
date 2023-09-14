f498ebb1730e20e108ce529d21705c110ba1cbb5
AquillaAf <aquillaafintinni@gmail.com>
2023-09-14 21:35:17 +0000

    strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,7 +6,7 @@
 * @separator: seperates the string
 * @n: number of strings
 *
-*Return: 
+*Return:
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -15,15 +15,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *point;
 
 	va_start(str, n);
-	for(i = 0; i < n; i++)
- 	{
+	for (i = 0; i < n; i++)
 		point = va_arg(str, char*);
 		if (point == NULL)
 		printf("(nil)");
 		else
 		printf("%s", point);
-		if (i != (n- 1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 		printf("%s", separator);
-	}
+
 	printf("\n");
 }
