e5fef3bdbf022e3053b26553a87c55f6e8174bd2
AquillaAf <aquillaafintinni@gmail.com>
2023-09-14 19:46:22 +0000

    print numbers variadic fun

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		x = va_arg(num, unsigned int);
 		printf("%d", x);
 		if (i != (n - 1) && separator != NULL)
-		printf("%s ", separator);
+		printf("%s", separator);
 	}
 	printf("\n");
 	va_end(num);
