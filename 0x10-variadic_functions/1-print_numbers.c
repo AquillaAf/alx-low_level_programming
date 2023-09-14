27bb17b9b0bafbfb3efa36b14b8466f523159674
AquillaAf <aquillaafintinni@gmail.com>
2023-09-14 16:38:07 +0000

    print numbers variadic fun

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* print_numbers - function takes varaidic arguments, prints num
+* @separator: string to be printed between num
+* @n: number of int passed to the function
+*
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int x;
+	unsigned int i;
+	separator = ",";
+
+	va_start(num, n);
+	for(i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			continue;
+		else
+		{
+			x = va_arg(num, unsigned int);
+			printf("%d", x);
+			if (i != (n - 1))
+			printf("%s ", separator);
+		}
+	}
+	printf("\n");
+}
