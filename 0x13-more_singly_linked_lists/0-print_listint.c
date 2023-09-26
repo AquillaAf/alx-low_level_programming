308da21c65157d3da7ff76482acadcaa5dbd971a
AquillaAf <aquillaafintinni@gmail.com>
2023-09-26 11:13:11 +0000

    function prints the lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - print all the lnked lists
+ * @h: variable pointer to each node
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t var = 0;
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		var++;
+	}
+	return (var);
+}
