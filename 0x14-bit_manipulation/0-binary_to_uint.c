eb3242e4d62c234253015dc15117c08e42721f16
AquillaAf <aquillaafintinni@gmail.com>
2023-11-24 20:55:14 +0000

    bin to decimal converter

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,11 @@ unsigned int binary_to_uint(const char *b)
 
 	while (b[i] != '\0')
 	{
-		if (b[i] != '0' && b[i] !='1')
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		if (b == NULL)
 		{
 			return (0);
 		}
