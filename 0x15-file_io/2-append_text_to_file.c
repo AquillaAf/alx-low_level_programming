6b492e66e4db22ab0481549d8d5eebb7dedbd99a
AquillaAf <aquillaafintinni@gmail.com>
2023-12-25 10:18:16 +0000

    fn appends text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include "main.h"
+/**
+ * append_text_to_file - file append text to existing file
+ * @filename: pointer to the file
+ * @text_content: text to append
+ *
+ * Return: 1 if successful -1 if it fails
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op;
+	int wr;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	op = open(filename, O_WRONLY | O_EXCL | O_APPEND);
+	wr = write(op, text_content, strlen(text_content));
+
+	if (op == -1 || wr == -1)
+	{
+		return (-1);
+	}
+
+	close(op);
+	return (1);
+}
