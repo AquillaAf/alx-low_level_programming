b9839950d9bdb42641a0468b93d42cdfc302b541
AquillaAf <aquillaafintinni@gmail.com>
2023-12-25 10:08:46 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,14 +24,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[count] != '\0')
 			count++;
-
 	}
 	else
 	{
 		close(j);
 		return (1);
 	}
-	k = write(j, text_content, strlen(text_content));
+	k = write(j, text_content, count);
 	if (j != count || k == -1)
 	{
 		return (-1);
