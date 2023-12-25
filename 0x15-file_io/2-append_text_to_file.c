453c8667fa11dc0c5393161f330ddb1dd77a8387
AquillaAf <aquillaafintinni@gmail.com>
2023-12-25 10:25:01 +0000

    fn appends text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,14 +10,22 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int op;
-	int wr;
+	int wr, count = 0;
+
+	if (text_content != NULL)
+	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	else
+		return (1);
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 	op = open(filename, O_WRONLY | O_EXCL | O_APPEND);
-	wr = write(op, text_content, strlen(text_content));
+	wr = write(op, text_content, count);
 
 	if (op == -1 || wr == -1)
 	{
