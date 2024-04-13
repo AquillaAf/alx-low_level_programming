ea1aec1fc9c144eab581b10953e664bfc55324de
AquillaAf <aquillaafintinni@gmail.com>
2024-04-13 21:32:09 +0000

    getting index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+#include <string.h>
+/*
+ * key_index - function that gives us the index of a key
+ * @key: a string lateral of the key
+ * @size: the size of the array of the hash table
+ *
+ * Return: the index where the key/value is stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = hash_djb2(key) % size;
+	return index;
+}
