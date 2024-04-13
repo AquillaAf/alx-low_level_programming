6b8899c3cfebc952d9e67ba3d37afa5c664f381c
AquillaAf <aquillaafintinni@gmail.com>
2024-04-13 16:44:52 +0000

    creating hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,16 +4,15 @@
  * hash_table_create - a function pointer to create a hash table
  *
  * @size: size of the table
+ *
  * Return: a pointer to the table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
-	
 	if (table == NULL)
 		return NULL;
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
-		
+	table->array = calloc(table->size, sizeof(hash_node_t*));	
 return table;
 }
